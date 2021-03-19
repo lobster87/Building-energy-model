aa07c6a4d9d3eb5399fd09e46d07b1361f6fb0f0
Frank <frankiepk@outlook.com>
2021-03-19 23:32:19 +0000

    changed main and produced hourly temperature

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from Create data import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-does()
+temperature_data = pd.read_csv('Temperature data.csv')
+
+df = pd.DataFrame(temperature_data)
+x = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+y = df[0,:]
+plt.plot(x,y)
+plt.show()
 
 
 
