511041627fde8d29f87b882357c4c467dc039719
Frankie <frankiepk@outlook.com>
2021-03-20 12:48:25 +0000

    updated files

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-temperature_data = pd.read_csv('Temperature data.csv')
+temperature_data = pd.read_csv('HourlyTemperature.csv')
 
 df = pd.DataFrame(temperature_data)
-x = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
-y = df[0,:]
-plt.plot(x,y)
-plt.show()
+print(df[1,1])
+print(df)
+#x = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+#y = df[0,:]
+#plt.plot(x,y)
+#plt.show()
 
 
 
